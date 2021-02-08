f0e52d9960f5993e1ebc978e488a093468f2ede3
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-02-08 14:12:34 +0000

    Pass container registry url to git server

--- environments/develop/rancher-bootstrap/main.tf
+++ environments/develop/rancher-bootstrap/main.tf
@@ -14,11 +14,12 @@ module "rancher_server" {
   environment            = "dev"
   project                = "rancher-server"
   suffix                 = "z1"
-  git_server_version     = "2.20"
+  git_server_version     = "2.25"
   azure_region           = "ukwest" # ukwest
   dns_zone_name          = "dev.icap-proxy.curlywurly.me"
   subscription_id        = "b8177f86-515f-4bff-bd08-1b9535dbc31b"
   tenant_id              = "7049e6a3-141d-463a-836b-1ba40d3ff653"
   key_vault_resource_group = "keyvault"
   key_vault_name           = "gw-icap-keyvault"
+  container_registry_url   = "gwicapcontainerregistry.azurecr.io/"
 }
